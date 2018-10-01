3e593e964a1ceda36e328af968e752f715b580d8
Ashley <ashleymargallo1@gmail.com>
2018-10-01 17:25:43 -0600

    multiple buttons to change state & sound

--- Exercises/dj_react/src/App.js
+++ Exercises/dj_react/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Grid from './Grid';
+import './styles.css';
+
 
 function App() {
   return(
--- Exercises/dj_react/src/Grid.js
+++ Exercises/dj_react/src/Grid.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './styles.css';
+import React, { Component, createRef } from 'react';
+import audioClip from "./assets/audio/Pen_Clicks-Simon_Craggs-514025171.mp3";
 
 class Grid extends Component {
   constructor() {
@@ -10,6 +10,7 @@ class Grid extends Component {
       box3: "white",
       box4: "white"
     }
+    this.audio = createRef();
     this.blackWhite = this.blackWhite.bind(this);
     this.purple = this.purple.bind(this);
     this.blueLeft = this.blueLeft.bind(this);
@@ -20,14 +21,15 @@ class Grid extends Component {
     this.teal4 = this.teal4.bind(this);
   }
   blackWhite() {
-    this.setState(prevState => ({
-      box1: prevState.box1 = "black",
-      box2: prevState.box2 = "black",
-      box3: prevState.box3 = "black",
-      box4: prevState.box4 = "black"
-    }))
+    this.setState({
+      box1: "black",
+      box2: "black",
+      box3: "black",
+      box4: "black"
+    })
   }
   purple() {
+    this.audio.current.play();
     this.setState(prevState => ({
       box1: prevState.box1 = "purple",
       box2: prevState.box2 = "purple",
@@ -64,7 +66,6 @@ class Grid extends Component {
     }))
   }
   render() {
-    const { change } = this.state;
     const styles = {
       styles1: {
         backgroundColor: this.state.box1
@@ -79,30 +80,28 @@ class Grid extends Component {
         backgroundColor: this.state.box4
       }
     }
-    return(
+    return (
       <div>
-        <div class="grid">
-          <div class="box" style={ styles.styles1 }>1</div>
-          <div class="box" style={ styles.styles2 }>2</div>
-          <div class="box" style={ styles.styles3 }>3</div>
-          <div class="box" style={ styles.styles4 }>4</div>
+        <div className="grid">
+          <div className="box" style={styles.styles1}>1</div>
+          <div className="box" style={styles.styles2}>2</div>
+          <div className="box" style={styles.styles3}>3</div>
+          <div className="box" style={styles.styles4}>4</div>
         </div>
-        <div class="button-grid">
-          <button class="black-white" onClick={this.blackWhite}>Black or White</button>
-          <button class="purple" onClick={this.purple}>Purple</button>
-          <button class="blue-left" onClick={this.blueLeft}>Blue Left</button>
-          <button class="blue-right" onClick={this.blueRight}>Blue Right</button>
-          <button class="lavender1" onClick={this.lavender1}>Lavender</button>
-          <button class="pink2" onClick={this.pink2}>Pink</button>
-          <button class="grey3" onClick={this.grey3}>Grey</button>
-          <button class="teal4" onClick={this.teal4}>Teal</button>
+        <div className="button-grid">
+          <button className="black-white" onClick={this.blackWhite}>Black or White</button>
+          <audio ref={this.audio} src={audioClip} type="audio/mp3"></audio>
+          <button className="purple" onClick={this.purple}>Purple</button>
+          <button className="blue-left" onClick={this.blueLeft}>Blue Left</button>
+          <button className="blue-right" onClick={this.blueRight}>Blue Right</button>
+          <button className="lavender1" onClick={this.lavender1}>Lavender</button>
+          <button className="pink2" onClick={this.pink2}>Pink</button>
+          <button className="grey3" onClick={this.grey3}>Grey</button>
+          <button className="teal4" onClick={this.teal4}>Teal</button>
         </div>
       </div>
     )
   }
 }
-
-
-
 
 export default Grid;
