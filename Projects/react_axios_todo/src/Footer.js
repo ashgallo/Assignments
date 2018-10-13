f73f1d6bf512cc56d9ac134ef292ee888bb4cbe9
Ashley <ashleymargallo1@gmail.com>
2018-10-12 20:20:46 -0600

    axios todo react app

---
+++ Projects/react_axios_todo/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <footer>© AshGallo, 2018</footer>
+  )
+}
+
+export default Footer;
---
+++ Projects/react_axios_todo/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>Hello... <br/> Let me help you keep track of all you need to do</header>
+  )
+}
+
+export default Header;
