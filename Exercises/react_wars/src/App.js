123eba987949af79e3df5000bebe8bfdd9367b3a
Ashley <ashleymargallo1@gmail.com>
2018-10-15 21:10:10 -0600

    component edits

--- Exercises/react_wars/src/App.js
+++ Exercises/react_wars/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Characters from "./Characters";
 
 function App() {
--- Exercises/react_wars/src/Characters.js
+++ Exercises/react_wars/src/Characters.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import axios from "axios";
-
 import CharacterView from "./CharacterView";
 
 export default class Characters extends Component {
